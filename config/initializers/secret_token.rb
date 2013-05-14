32477163b608cc5bdd660b87429d5e76f95d7264
vicoar <vico.ar@gmail.com>
2013-05-13 21:28:32 -0300

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iebidata::Application.config.secret_token = '12b775b61ce7f4aa055dc91ddc6b9570b8fbf9a1a21642f7680f0078f368b3e189d426f8aabe575a3ab0ee5862438252a59ecdd6d480482f6fd6496a76edb6df'
